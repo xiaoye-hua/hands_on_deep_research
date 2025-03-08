ead8da2192bc52ba7e74419d5683cfa4219e3941
xiaoye_hua <huag@kth.se>
2025-03-08 10:29:30 +0100

    remove unrelated files

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Hands-On Deep Research Package.
-
-A Python framework for conducting deep research with multi-agent systems.
-"""
-
-__version__ = "0.1.0" 
--- src/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utility functions for the Hands-On Deep Research project.
-"""
-
-from src.utils.api import get_api_key
-from src.utils.logging import setup_logging
-
-__all__ = ["get_api_key", "setup_logging"] 
